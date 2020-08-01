1bca6a0e4cf812eb9755767348856ac8aea611d8
Krishna Chandhok <krishnachandhok04@gmail.com>
2020-08-02 02:30:52 +0530

    My first commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_30_195739) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "summary"
